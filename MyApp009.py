b1877d5cf72edcb9d8b08b95bdf1cd808fb5c2a1
nhunwasika <129248207+nhunwasika@users.noreply.github.com>
2023-11-21 14:57:23 +0700

    .

--- MyApp009.py
+++ MyApp009.py
@@ -9,9 +9,9 @@ col1, col2 = st.columns(2)
 #col1.write("This is column 1")
 #col2.write("This is column 2")
 with col1:
-    st.image('./pic/pic5.png')
-#with col2:
-    #st.image('./pic/pic2.jpg')
+    st.image('./pic/pic1.jpg')
+with col2:
+    st.image('./pic/pic2.jpg')
 
 html_1 = """
 <div style="background-color:#ccccff;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
