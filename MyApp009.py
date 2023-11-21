3c0b8b83ef9d48ee87b733c65a6c2ee23a9c8669
nhunwasika <129248207+nhunwasika@users.noreply.github.com>
2023-11-21 14:27:54 +0700

    .

--- MyApp009.py
+++ MyApp009.py
@@ -12,3 +12,16 @@ with col1:
     st.image('./pic/pic1.jpg')
 #with col2:
     #st.image('./pic/pic2.jpg')
+
+html_1 = """
+<div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>ข้อมูลของลูกค้าทัวร์</h5></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
+
+import pandas as pd
+
+raw_data=pd.read_csv('./data/Tour_Travel.csv')
+st.write(raw_data.head(10))
