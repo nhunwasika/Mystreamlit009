2018817c776678debb6bdda7afca52ad41143a43
nhunwasika <129248207+nhunwasika@users.noreply.github.com>
2023-11-21 14:22:36 +0700

    .

--- MyApp009.py
+++ MyApp009.py
@@ -10,5 +10,5 @@ col1, col2 = st.columns(2)
 #col2.write("This is column 2")
 with col1:
     st.image('./pic/pic1.jpg')
-with col2:
-    st.image('./pic/pic2.jpg')
+#with col2:
+    #st.image('./pic/pic2.jpg')
